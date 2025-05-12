7ec4efd7c331a5d5c55a8b98c7da5307f1358939
Bary-G <b9059289@gmail.com>
2025-05-12 14:57:03 +0200

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,7 @@
 """Show numbers from 0 to 99"""
 num = 0
 while num <= 99:
-    if num < 10:
-        print(str.format("0"), end="")
-    print(str.format(f"{num}"), end=", ")
+    print(str.format(f"{num:02}"), end="")
+    if num != 99:
+        print(str.format(f", "), end="")
     num = num + 1
