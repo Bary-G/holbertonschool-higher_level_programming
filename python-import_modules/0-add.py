039ca85bbf4814f6de28eb068839d681f2bedd8a
Bary-G <b9059289@gmail.com>
2025-05-13 15:16:39 +0200

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Add 2 values by importing function from another file"""
 from add_0 import add
+"""Calculate 2 values by importing functions from another file"""
 a = 1
 b = 2
 print(str.format(f"{a} + {b} = {add(1, 2)}"))
---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import *
+"""Add 2 values by importing function from another file"""
+a = 15
+b = 5
+print(str.format(f"{a} + {b} = {add(a, b)}"))
+print(str.format(f"{a} - {b} = {sub(a, b)}"))
+print(str.format(f"{a} * {b} = {mul(a, b)}"))
+print(str.format(f"{a} / {b} = {div(a, b)}"))
