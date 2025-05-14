e728415ccf3ee86e4f391bcdcccf1db3194ca209
Bary-G <b9059289@gmail.com>
2025-05-14 09:53:13 +0200

    1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import __main__
 from calculator_1 import *
-"""Add 2 values by importing function from another file"""
-a = 15
-b = 5
-print(str.format(f"{a} + {b} = {add(a, b)}"))
-print(str.format(f"{a} - {b} = {sub(a, b)}"))
-print(str.format(f"{a} * {b} = {mul(a, b)}"))
-print(str.format(f"{a} / {b} = {div(a, b)}"))
+"""Calculate 2 values by importing functions from another file"""
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(str.format(f"{a} + {b} = {add(a, b)}"))
+    print(str.format(f"{a} - {b} = {sub(a, b)}"))
+    print(str.format(f"{a} * {b} = {mul(a, b)}"))
+    print(str.format(f"{a} / {b} = {div(a, b)}"))
