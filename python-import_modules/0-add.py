e6dbff294fb375a65a35f39a86fbb5b6c836cbce
Bary-G <b9059289@gmail.com>
2025-05-14 09:37:57 +0200

    0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import __main__
-from add_0 import add
+from operator import add
 """Calculate 2 values by importing functions from another file"""
-a = 1
-b = 2
-print(str.format(f"{a} + {b} = {add(1, 2)}"))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print(str.format(f"{a} + {b} = {add(1, 2)}"))
