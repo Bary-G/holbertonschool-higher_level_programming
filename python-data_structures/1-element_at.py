78232a22a93ed2647882f76817876d5384ef813d
Bary-G <b9059289@gmail.com>
2025-05-15 10:39:21 +0200

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """Index a variable in a list"""
-    if idx < 0:
+    if not isinstance(idx, int):
         return None
-    elif idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    else:
-        return my_list[idx]
+    return my_list[idx]
