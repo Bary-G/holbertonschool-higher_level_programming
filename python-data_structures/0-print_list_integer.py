9d39b235fea602a866111901a9a258c33548331e
Bary-G <b9059289@gmail.com>
2025-05-15 09:09:47 +0200

    0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """Prints each integer in the list on a new line using str.format()"""
+    """Prints each integer from the list using str.format()"""
     for i in my_list:
         print("{:d}".format(i))
