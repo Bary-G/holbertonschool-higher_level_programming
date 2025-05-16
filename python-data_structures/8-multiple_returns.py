3bca6aff38a50c17681193c23f7040fb4cf9b2e5
Bary-G <b9059289@gmail.com>
2025-05-16 11:02:32 +0200

    8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     """Return the length of a string and its first character"""
-    if not sentence:
-        return None
+    if not sentence or not isinstance(sentence, str):
+        return 0, None
     sentence_len = len(sentence)
     first_char = sentence[0]
     return sentence_len, first_char
