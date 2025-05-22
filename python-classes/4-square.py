348f4ce6ae1d883fdff281082adef0d138b5bff7
Bary-G <b9059289@gmail.com>
2025-05-22 10:28:05 +0200

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Module:
+A Square class that represents... a square.
+"""
+
+
+class Square:
+    """
+    Represents a square shape with attributes and methods to define its
+    properties.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square with a given size.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+
+    def size(self):
+        """
+        Gets the size of the square.
+        """
+        return self._size
+
+    def size(self, value):
+        """
+        Sets the size of the square with basic validation.
+        """
+        if not isinstance(value, int) or value < 0:
+            raise ValueError
+        self._size = value
+
+    def area(self):
+        """
+        Return the area of the square.
+        """
+        if not isinstance(self.size, int):
+            raise TypeError("size must be an integer")
+        elif self.size < 0:
+            raise ValueError("size must be >= 0")
+        return self.size * self.size
