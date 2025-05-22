69b8f2b2ad4830c8e8563dabf18cdfba96654faf
Bary-G <b9059289@gmail.com>
2025-05-22 10:47:34 +0200

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -41,4 +41,6 @@ class Square:
         """
         if not isinstance(self.size, int):
             raise TypeError("size must be an integer")
+        elif self.size < 0:
+            raise ValueError("size must be >= 0")
         return self.size * self.size
