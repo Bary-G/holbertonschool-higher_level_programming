9f659fcc3b452de5174fd917e3b1c69f40539331
Bary-G <b9059289@gmail.com>
2025-05-23 09:50:58 +0200

    1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Module:
+A Rectangle class that represents a rectangular shape.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangular shape with attributes and methods to define its
+    properties.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes the rectangle with a given size.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Gets the width of the rectangle.
+        """
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """
+        Sets the width of the rectangle with basic validation.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self._Rectangle__width = value
+
+    @property
+    def height(self):
+        """
+        Gets the height of the rectangle.
+        """
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """
+        Sets the height of the rectangle with basic validation.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self._Rectangle__height = value
