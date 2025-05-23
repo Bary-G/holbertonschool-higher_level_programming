f416dd02812f68f462bdb467be4537f17f786fa3
Bary-G <b9059289@gmail.com>
2025-05-23 09:29:47 +0200

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module:
+A class that represents a Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangular shape with attributes and methods to define its
+    properties.
+    """
