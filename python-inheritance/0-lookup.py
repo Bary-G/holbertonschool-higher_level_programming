b97b5f13b5ece3092c1972bb05c0b56cba6ddfa4
Bary-G <b9059289@gmail.com>
2025-05-26 11:23:26 +0200

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,10 +4,10 @@ Module:
 A Python script that contain the lookup function.
 """
 
+
 def lookup(obj):
     """
     A function that returns the list of available attributes
     and methods of an object.
     """
     return (dir(obj))
-    
