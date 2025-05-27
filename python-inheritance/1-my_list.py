0fb7ebd7286e6951ddd9a05d4ce66a04f16d7486
Bary-G <b9059289@gmail.com>
2025-05-27 14:44:21 +0200

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,9 +9,10 @@ class MyList(list):
     """
     A class that inherits from list.
     """
-
     def print_sorted(self):
         """
         A method that sorts a list of integers in ascending order.
         """
+        if not all(isinstance(item, int) for item in self):
+            raise TypeError("All elements in the list must be of type int.")
         print(sorted(self))
