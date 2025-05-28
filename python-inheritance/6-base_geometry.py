060f6a70a682520697015b2387e0f0f4cc88ca2d
Bary-G <b9059289@gmail.com>
2025-05-28 11:05:35 +0200

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module:
+A Python script that contains the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class that represents a geometric base.
+    """
+    def area(self):
+        """
+        A method that raises an Exception with a message
+        """
+        raise Exception("area() is not implemented")
