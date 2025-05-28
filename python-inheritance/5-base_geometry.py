146b3f07b1dac19d09b45891a6657221078fb313
Bary-G <b9059289@gmail.com>
2025-05-28 10:12:30 +0200

    5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module:
+A Python script that contains the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A class that represents a geometric base.
+    """
