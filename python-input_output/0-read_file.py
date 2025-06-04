52a5ef3a685dfd6fbd9ee9a6b0c90a2f47d02a3b
Bary-G <b9059289@gmail.com>
2025-06-04 09:55:13 +0200

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
     A function that reads a text file.
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
