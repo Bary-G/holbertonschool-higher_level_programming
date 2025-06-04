8c86fd34a33c83980837b932dc3a592c6c67f172
Bary-G <b9059289@gmail.com>
2025-06-04 10:51:35 +0200

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,6 +9,13 @@ def write_file(filename="", text=""):
     """
     A function that write in a text file.
     """
+    try:
+        with open(filename, "r"):
+            pass
+    except FileNotFoundError:
+        with open(filename, "w"):
+            pass
+
     with open(filename, "a") as f:
         f.write(text)
         return len(text)
