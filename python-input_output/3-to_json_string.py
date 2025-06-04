4642cc21e9001a9efbe44f4109a04f31eed76dfc
Bary-G <b9059289@gmail.com>
2025-06-04 12:52:24 +0200

    1

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""
+Module:
+A file that runs Python functions.
+"""
+
+
+def to_json_string(my_str):
+    """
+    A function that return a Python data structure into JSON string.
+    """
+    return json.dumps(my_str)
