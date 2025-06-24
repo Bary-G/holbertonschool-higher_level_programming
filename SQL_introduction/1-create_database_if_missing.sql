d62ea40f77f0fffcb85d8140c716cd999bf42945
Bary-G <b9059289@gmail.com>
2025-06-24 11:23:12 +0200

    1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,3 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
-
+-- hbtn_0c_0
+-- Create my new database
+CREATE DATABASE hbtn_0c_0;
