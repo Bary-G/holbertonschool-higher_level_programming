ea729bb49d643dccef708fe1a2b14d62d582d737
Bary-G <b9059289@gmail.com>
2025-08-06 14:33:00 +0200

    0

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-// Prints a message depending of the number of arguments passed
+// Prints a message depending on the number of arguments passed
+
 function checkArgs() {
 	const args = process.argv.slice(2);
 	if (args.length === 0) {
-		console.log("No argument");
+		console.log('No argument');
 	} else if (args.length === 1) {
-		console.log("Argument found");
+		console.log('Argument found');
 	} else {
-		console.log("Arguments found");
+		console.log('Arguments found');
 	}
 }
 
