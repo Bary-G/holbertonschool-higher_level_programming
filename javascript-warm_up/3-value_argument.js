3aeeed4697f30e101f1f7962e2f359998fd1a2b8
Bary-G <b9059289@gmail.com>
2025-08-06 15:13:31 +0200

    0

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Prints the first argument passed to it
+
+function checkArgs() {
+  const args = process.argv.slice(2);
+  if (args[0] === undefined) {
+  console.log('No argument');
+  } else {
+    console.log(args[0]);
+  }
+}
+
+checkArgs();
