0ebcab85573092d497a588837b93a76c363cb515
Bary-G <b9059289@gmail.com>
2025-08-07 10:21:44 +0200

    0

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// Concat two arguments passed to it
+
+function checkArgs () {
+  const args = process.argv.slice(2);
+  console.log(`${args[0]} is ${args[1]}`);
+}
+
+checkArgs();
