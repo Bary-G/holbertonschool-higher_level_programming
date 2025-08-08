2643c49a452b4b833b5e809847b9f8e572c39d3b
Bary-G <b9059289@gmail.com>
2025-08-08 14:34:10 +0200

    0

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,16 +2,16 @@
 // A script that prints x times 'C is fun'
 
 function checkArgs () {
-  const args = process.argv.slice(2)
-  const num = parseInt(args[0])
-  let index = 1
+  const args = process.argv.slice(2);
+  const num = parseInt(args[0]);
+  let index = 1;
 
   if (isNaN(num)) {
-    console.log('Missing number of occurrences')
+    console.log('Missing number of occurrences');
   } else {
     while (index <= num) {
-      console.log('C is fun')
-      index += 1
+      console.log('C is fun');
+      index += 1;
     }
   }
 }
