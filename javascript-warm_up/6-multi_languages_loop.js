07eb2044224efa8abaee6035e22ce8e3477d38be
Bary-G <b9059289@gmail.com>
2025-08-08 14:11:34 +0200

    0

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+// Prints the full array
+
+function checkArgs () {
+  const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+  let index = 0;
+  while (arr[index]) {
+    console.log(arr[index]);
+    index = index + 1;
+  }
+}
+
+checkArgs();
