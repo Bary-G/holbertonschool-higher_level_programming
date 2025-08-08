b610eb279f3a49cd4e2357b89cd25694becce9bd
Bary-G <b9059289@gmail.com>
2025-08-08 11:19:10 +0200

    0

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+// Convert string into integer if the first argument can be converted
+
+function checkArgs () {
+  const args = process.argv.slice(2);
+  const num = parseInt(args[0]);
+
+  if (isNaN(num)) {
+    console.log('Not a number');
+  } else {
+    console.log(`My number: ${num}`);
+  }
+}
+
+checkArgs();
