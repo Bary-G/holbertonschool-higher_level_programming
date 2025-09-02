db537f6adb0763723485d205ecf2175924492d63
Bary-G <b9059289@gmail.com>
2025-09-02 09:07:03 +0200

    Task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,13 +8,13 @@ random: This module is used for generating random values
 
 number = random.randint(-10000, 10000)
 if number >= 0:
-    lastdigit = number%10
+    lastdigit = number % 10
 else:
-    lastdigit = -(abs(number)%10)
+    lastdigit = -(abs(number) % 10)
 print(f"Last digit of {number} is {lastdigit}", end=" ")
 if lastdigit > 5:
     print("and is greater than 5")
 elif lastdigit == 0:
     print("and is 0")
-elif lastdigit < 6 and number%10 != 0:
+elif lastdigit < 6 and number % 10 != 0:
     print("and is less than 6 and not 0")
