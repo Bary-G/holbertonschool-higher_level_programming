13b0c4de134b1cbe545cced9babc2f3dd2f2f3ab
Bary-G <b9059289@gmail.com>
2025-09-02 08:58:29 +0200

    Tasks from 1 to 3

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+character = 97
+while character < 123:
+    print(f"{chr(character)}", end="")
+    character = character + 1
---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+character = 97
+while character < 123:
+    if character != 113 and character != 101:
+        print(f"{chr(character)}", end="")
+    character = character + 1
