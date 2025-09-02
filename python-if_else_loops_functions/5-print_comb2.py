83ecf691960a5e7adda79cf4074d8c5baa28637a
Bary-G <b9059289@gmail.com>
2025-09-02 12:02:45 +0200

    Task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 number1 = 0
 number2 = -1
-while number1 < 99 :
+while (number1 * 10) + number2 < 99:
     number2 = number2 + 1
     if number2 == 10 and number1 != 10 :
         number2 = number2 - 10
         number1 = number1 + 1
-    print("{}{}".format(number1, number2), end="")
+    if (number1 * 10) + number2 == 99:
+        print("{}{}".format(number1, number2))
+    else:
+        print("{}{}".format(number1, number2), end=", ")
