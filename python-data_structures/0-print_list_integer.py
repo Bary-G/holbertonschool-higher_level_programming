ae257733d7fce27d8c2038dde01d01bb6f57e51f
Bary-G <b9059289@gmail.com>
2025-09-08 09:14:04 +0200

    Task 00

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -5,5 +5,4 @@ def print_list_integer(my_list=[]):
     A function that prints all integers of a list.
     """
     for index in my_list:
-        str = "{}"
-        print(str.format(my_list[index - 1]))
+        print("{}".format(my_list[index - 1]))
