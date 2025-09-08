bcd3d4d0435cdd09a95e021bd1d1fe9d6c30effc
Bary-G <b9059289@gmail.com>
2025-09-08 11:14:03 +0200

    Task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    A function that prints a matrix of integers.
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            last_chr = " "
+            if j == len(matrix[i]) - 1:
+                last_chr = ""
+            print("{}".format(matrix[i][j]), end=last_chr)
+        print("{}".format("\n"), end="")
