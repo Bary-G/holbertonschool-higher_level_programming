0b7b9e0466df72b3634675966df561891d937c16
Bary-G <b9059289@gmail.com>
2025-09-09 09:52:15 +0200

    Task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -5,4 +5,6 @@ def multiple_returns(sentence):
     A function that returns a tuple with the length of a string and its first
     character.
     """
+    if len(sentence) <= 0:
+        return(0, None)
     return(len(sentence), sentence[0])
