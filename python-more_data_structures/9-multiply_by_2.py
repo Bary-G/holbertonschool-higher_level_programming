dbcf7bcaaed56f169c6edd23790067144f2db5a2
Bary-G <b9059289@gmail.com>
2025-09-10 14:38:44 +0200

    Task 9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """
+    A function that returns a new dictionary with
+    all values multiplied by 2
+    """
+    new_dict = dict(a_dictionary)
+    for key in new_dict:
+        new_dict[key] *= 2
+    return new_dict
