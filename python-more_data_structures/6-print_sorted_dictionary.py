6685ad466c5630e5fad7e20115ba454f83c39bfa
Bary-G <b9059289@gmail.com>
2025-09-10 11:16:27 +0200

    Task 7

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    A function that prints a dictionary by ordered keys.
+    """
+    sorted_dict = {key: a_dictionary[key] for key in sorted(a_dictionary)}
+    for key, value in sorted_dict.items():
+        print(f"{key} : {value}")
