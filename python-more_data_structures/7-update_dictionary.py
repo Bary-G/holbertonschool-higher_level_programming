bf216ab6fa5fc52d660a39efc4b0bab13422113d
Bary-G <b9059289@gmail.com>
2025-09-10 11:19:54 +0200

    Task 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -4,7 +4,7 @@ def update_dictionary(a_dictionary, key, value):
     """
     A function that replaces or adds key/value in a dictionary.
     """
-    if not isinstance(key, str) or value == None:
+    if not isinstance(key, str) or value is None:
         return a_dictionary
     a_dictionary[key] = value
     return a_dictionary
