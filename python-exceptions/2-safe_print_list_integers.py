ce070f384468bb9128cd247aad67417b0a59bc61
Bary-G <b9059289@gmail.com>
2025-09-12 10:21:51 +0200

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,12 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
     A function that display the first x elements of a list and only integers.
     """
     count = 0
-    try:
-        for item in my_list:
-            if count < x and isinstance(item, int):
-                print("{:d}".format(item), end="")
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
                 count += 1
-        print("")
-        return count
-    except IndexError:
-        return count
+        except TypeError:
+            continue
+    print("")
+    return count
