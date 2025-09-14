6625793c3b58910a07f845bc9b14909e4a0bd0ea
Bary-G <b9059289@gmail.com>
2025-09-14 21:17:08 +0200

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@ def add_integer(a, b=98):
     """
     A function that adds 2 integers.
     """
-    if not isinstance (a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance (b, (int, float)):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
