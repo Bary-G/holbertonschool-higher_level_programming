6d95a954c6feaf0a4cdf25cbd6b4b7f73a1eeea2
Bary-G <b9059289@gmail.com>
2025-09-14 21:20:45 +0200

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
+"""
+Module: A Python script file with functions.
+"""
 
 def add_integer(a, b=98):
     """
     A function that adds 2 integers.
-    
-    Parameters:
-    a: int or float - First number to add
-    b: int or float - Second number to add (default is 98)
-
-    Returns:
-    int - The sum of a and b as integers
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
