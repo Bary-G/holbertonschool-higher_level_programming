df49414700064386275672f2829a979a70e832bd
Bary-G <b9059289@gmail.com>
2025-09-15 10:01:37 +0200

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: A Python script file with functions.
+"""
+
+
+class Square():
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size):
+        """
+        All privates instances.
+        """
+        self._Square__size = size
