90263041f39d480971b9de20c5dae5111974be6c
Bary-G <b9059289@gmail.com>
2025-09-15 10:04:31 +0200

    Task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: A Python script file with functions.
+"""
+
+
+class Square():
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size=0):
+        """
+        All privates instances.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
