14cf3b6ad79f4bf2794847b22bd2b34d0ef610c4
BaryG <b9059289@gmail.com>
2025-09-22 14:50:52 +0200

    Task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: A python Script that runs functions.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is exactly an instance
+    of the specified class ; otherwise False.
+    """
+    if not obj or obj is None:
+        raise Exception("obj is mandatory and must be not None")
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
