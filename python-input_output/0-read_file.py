5967af2f84aafb180879a5af8171d3bcb42182b4
Bary-G <b9059289@gmail.com>
2025-09-29 10:30:33 +0200

    Task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module : A Python file that tuns functions.
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
