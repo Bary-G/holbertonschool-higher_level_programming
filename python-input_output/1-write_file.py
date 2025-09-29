9ff1bff30f90828405fbedafdfadfcf7a90d081c
Bary-G <b9059289@gmail.com>
2025-09-29 11:05:44 +0200

    Task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module : A Python file that tuns functions.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        count = file.write(text)
+    return count
