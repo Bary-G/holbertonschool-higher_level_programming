ea2efeeff32d8248e5babc1f4afa7685ab723c3c
Bary-G <b9059289@gmail.com>
2025-09-30 10:46:16 +0200

    Task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 """
 Module : A Python file that runs functions.
-json : A Python module to convert data.
+json : A Python module to convert into JSON.
 """
+import json
 
 
 def to_json_string(my_obj):
-    """Returns the converted representation of an object (string)"""
+    """Returns the JSON representation of an object (string)"""
     return json.dumps(my_obj)
