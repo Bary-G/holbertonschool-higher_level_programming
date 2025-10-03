74b00b08477b0318e53740a5b2b9f53762d2550a
Bary-G <b9059289@gmail.com>
2025-10-03 09:27:53 +0200

    Task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -10,14 +10,6 @@ class Student:
     """
     def __init__(self, first_name, last_name, age):
         """Class instantiation"""
-        if not isinstance(first_name, str):
-            raise TypeError("first_name must be a string")
-        if not isinstance(last_name, str):
-            raise TypeError("last_name must be a string")
-        if not isinstance(age, int):
-            raise TypeError("age must be an integer")
-        if age <= 0:
-            raise TypeError("age must be more than 0")
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -27,8 +19,4 @@ class Student:
         Return the dictionary description with simple data structure 
         for JSON serialization of an object
         """
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        return self.__dict__
