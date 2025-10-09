327b0a9080ae359b6b6e23d22c5889a3c39e7e27
Bary-G <b9059289@gmail.com>
2025-10-09 11:41:12 +0200

    Task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,18 +3,38 @@
 Module : A Python file that runs functions.
 http : A library used for web functions.
 server : A module used for basic web server.
+json : A library used for web functions.
 """
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 
 
 class MyServer(BaseHTTPRequestHandler):
     """A basic HTML server"""
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain; charset=utf-8")
-        self.end_headers()
-        message = "Hello, this is a simple API!"
-        self.wfile.write(message.encode("utf-8"))
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain; charset=UTF8")
+            self.end_headers()
+            message = "Hello, this is a simple API!"
+            self.wfile.write(message.encode("UTF8"))
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json; charset=UTF8")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.wfile.write(json_data.encode("UTF8"))
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json; charset=UTF8")
+            self.end_headers()
+            info = {"version": "1.0", "description": "A simple API built with http.server"}
+            json_info = json.dumps(info)
+            self.wfile.write(json_info.encode("UTF8"))
+        else:
+            return self.send_response(404)
+
 
 host = "localhost"
 port = 8000
