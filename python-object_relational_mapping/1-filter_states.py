dc3c729265c749473acd787734988c060e38f1b6
Bary-G <b9059289@gmail.com>
2025-10-29 08:59:49 +0100

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module: A Python script that filters all states starting by letter N from a MySQL database.
+"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """Connect to MySQL and list all states in the database."""
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <username> <password> <database>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        for row in cursor.fetchall():
+            print(row)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as err:
+        print("MySQL Error:", err)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
