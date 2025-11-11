5ca05664035e01e83f44c64c870accbb67100308
Bary-G <b9059289@gmail.com>
2025-11-11 11:17:03 +0100

    Task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function factorial(n) {
+    if (n < 0) return undefined;
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+const args = process.argv.slice(2);
+console.log(factorial(Number(args[0])));
