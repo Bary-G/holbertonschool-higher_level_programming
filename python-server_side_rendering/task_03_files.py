af90b89c6ed32abfae29d9be198e73c1c5fcf88b
Bary-G <b9059289@gmail.com>
2025-11-30 15:46:49 +0100

    task 2

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
-"""
-Module to set up a basic web server using the http.server module
-"""
-import http.server
 import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class server(BaseHTTPRequestHandler):
+class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.data == "/":
+        if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type:", "application/json")
-            self.end_header()
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
             self.wfile.write("Hello, this is a simple API!")
-        elif self.data == "/data":
+        elif self.path == "/data":
             self.send_response(200)
-            self.send_header("Content-type:", "application/json")
-            self.end_header()
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data))
-        elif self.data == "/status":
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+        elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-type:", "application/json")
-            self.end_header()
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
             self.wfile.write("OK")
         else:
             self.send_response(404)
-            self.send_header("Content-type:", "application/json")
-            self.end_header()
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
             self.wfile.write("Endpoint not found")
 
 if __name__ == "__main__":
