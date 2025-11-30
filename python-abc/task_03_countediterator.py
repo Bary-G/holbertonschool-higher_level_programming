8af9d348b20feb8f1553da15fa2a7ae8fafe0fe8
Bary-G <b9059289@gmail.com>
2025-11-30 21:06:46 +0100

    task

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,15 @@
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+    
+    def get_count(self):
+        return self.counter
+    
+    def __next__(self):
+        try:
+            value = next(self.iterator)
+            self.counter += 1
+            return value
+        except StopIteration:
+            raise StopIteration
