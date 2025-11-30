2f2d6b0aaa88cdb953bc20a6d62ee8bdfee7e10e
Bary-G <b9059289@gmail.com>
2025-11-30 13:31:52 +0100

    task 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -8,40 +8,40 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain; charset=UTF-8")
+            self.send_header("Content-type", "text/plain; charset=utf-8")
             self.end_headers()
             message = "Hello, this is a simple API!"
-            self.wfile.write(message.encode("UTF8"))
+            self.wfile.write(message.encode("utf-8"))
 
         elif self.path == "/data":
             self.send_response(200)
-            self.send_header("Content-type", "application/json; charset=UTF-8")
+            self.send_header("Content-type", "application/json; charset=utf-8")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             json_data = json.dumps(data)
-            self.wfile.write(json_data.encode("UTF8"))
+            self.wfile.write(json_data.encode("utf-8"))
         
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-type", "text/plain; charset=UTF-8")
+            self.send_header("Content-type", "text/plain; charset=utf-8")
             self.end_headers()
             message = "OK"
-            self.wfile.write(message.encode("UTF8"))
+            self.wfile.write(message.encode("utf-8"))
 
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header("Content-type", "application/json; charset=UTF-8")
+            self.send_header("Content-type", "application/json; charset=utf-8")
             self.end_headers()
             info = {"version": "1.0", "description": "A simple API built with http.server"}
             json_info = json.dumps(info)
-            self.wfile.write(json_info.encode("UTF8"))
+            self.wfile.write(json_info.encode("utf-8"))
 
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/plain; charset=UTF-8")
+            self.send_header("Content-type", "text/plain; charset=utf-8")
             self.end_headers()
             error_message = "Endpoint not found"
-            self.wfile.write(error_message.encode("UTF8"))
+            self.wfile.write(error_message.encode("utf-8"))
 
 
 if __name__ == "__main__":
